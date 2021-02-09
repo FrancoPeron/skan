75ec735f53666dcf812a827f6ec2f35f34353f71
franco <francomatiasperon@gmail.com>
2021-02-08 21:38:18 -0300

    Inicio de projecto

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1 class="c-g4">{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your  dddddVue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+h1, h2 {
+  font-weight: normal;
+}
+</style>
